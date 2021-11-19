ea9818ddfa324cbb0f9850003e9550bfa8c3f0e7
Filip Tyborowski <filip.tyborowski@gmail.com>
2021-11-19 21:56:36 +0100

    CSharp introduce

---
+++ CSharp/7kyu/DescendingOrder.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Linq;
+
+namespace CSharp._7Kyu {
+    public static class DescendingOrder{
+        public static int Go(int n){
+            return int.Parse(string.Join("", n.ToString().OrderByDescending(a => a)));
+        }
+    }
+}
---
+++ CSharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CSharp._7Kyu;
+
+namespace CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            result = DescendingOrder.Go(15);
+            Console.WriteLine(result);
+        }
+    }
+}
