9f004071a9cdb94a3965ab6f7d5c4c3229c6277e
Filip Tyborowski <filip.tyborowski@gmail.com>
2022-08-16 21:39:11 +0200

    Common Denominator

--- CSharp/5kyu/Common Denominator.cs
+++ CSharp/5kyu/Common Denominator.cs
@@ -1,21 +1,66 @@
 using System.Linq;
 using System;
+using System.Collections.Generic;
 
 namespace CSharp._5kyu
 {
     public static partial class Kata5
     {
+        public static string convertFrac_kata_1(long[,] lst)
+        {
+            int c = lst.GetLength(0);
+            long l = 1;
+            for (var i = 0; i < c; i++) l = LCM(lst[i, 1], l);
+
+            return String.Empty;
+        }
+
+        // Last Common Multiple
+        private static long LCM(long a, long b)
+        {
+            var g = GCD(a, b);
+            return a * b / g;
+        }
+
+        // Greatest Common Divisor
+        private static long GCD(long a, long b)
+        {
+            if (b == 0) return a;
+
+            var m = a % b;
+            return GCD(b, m);
+        }
+
         public static string convertFrac(long[,] lst)
         {
+            var result = new List<string>();
             long m = 0;
-            for (int i = 0; i < lst.Length; i++)
-            { 
+            for (int i = 0; i < lst.GetLength(0); i++)
+            {
                 m = lst[i, 1] > m ? lst[i, 1] : m;
             }
 
-            Console.WriteLine(m);
+            var not_found = true;
+            while (not_found)
+            {
+                not_found = false;
+                for (int i = 0; i < lst.GetLength(0); i++)
+                {
+                    if (m % lst[i, 1] != 0)
+                    {
+                        not_found = true;
+                        m++;
+                        continue;
+                    }
+                }
+            }
 
-            return String.Empty;
+            for (int i = 0; i < lst.GetLength(0); i++)
+            {
+                result.Add($"({m / lst[i, 1] * lst[i, 0]},{m})");
+            }
+
+            return String.Join("", result);
         }
     }
 }
--- CSharp/5kyu/Testing/CommonDenominator_Tests.cs
+++ CSharp/5kyu/Testing/CommonDenominator_Tests.cs
@@ -11,7 +11,7 @@ namespace CSharp._5kyu
             long[,] lst = new long[,] { { 1, 2 }, { 1, 3 }, { 1, 4 } };
             var expected = "(6,12)(4,12)(3,12)";
 
-            var actual = Kata5.convertFrac(lst);
+            var actual = Kata5.convertFrac_kata_1(lst);
 
             Assert.Equal(expected, actual);
         }
